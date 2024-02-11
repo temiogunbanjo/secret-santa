2fac99ff98b7ec4e957d82edd6a18724c9034f16
Temiloluwa Ogunbanjo <ogunbanjotemiloluwa@gmail.com>
2024-02-11 07:40:17 +0100

    Responsiveness, speed and analytics

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -31,7 +31,7 @@ export default function Register() {
         {!hasCreatedSpace ? (
           <form
             onSubmit={submitHandler}
-            className="lg:relative flex flex-col gap-2 w-full lg:w-[300px] justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pt-6 pb-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 p-6 lg:dark:bg-zinc-800/30"
+            className="lg:relative flex flex-col gap-3 w-full lg:w-[300px] justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pt-6 pb-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 p-6 lg:dark:bg-zinc-800/30"
           >
             {/* Get started by editing&nbsp;
           <code className="font-mono font-bold">register/page.tsx</code> */}
