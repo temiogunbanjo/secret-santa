ac2fc79fbc87ec90c15a193e9341d46e54fd1a2d
Temiloluwa Ogunbanjo <ogunbanjotemiloluwa@gmail.com>
2024-02-11 07:41:11 +0100

    Responsiveness, speed and analytics

--- app/company/[organization-slug]/page.tsx
+++ app/company/[organization-slug]/page.tsx
@@ -50,7 +50,7 @@ export default function OrganizationHome() {
         <div className="flex flex-col-reverse gap-10 lg:gap-0 lg:flex-row z-10 lg:max-w-5xl w-full items-center justify-between font-mono text-sm mb-8">
           <form
             onSubmit={submitHandler}
-            className="lg:relative flex flex-col gap-2 w-full lg:w-[300px] justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pt-6 pb-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 p-6 lg:dark:bg-zinc-800/30"
+            className="lg:relative flex flex-col gap-3 w-full lg:w-[300px] justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pt-6 pb-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:border lg:bg-gray-200 p-6 lg:dark:bg-zinc-800/30"
           >
             {/* Get started by editing&nbsp;
           <code className="font-mono font-bold">register/page.tsx</code> */}
