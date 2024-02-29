4568caebe0c668fe7d62b1469e15d226da320a3b
Temiloluwa Ogunbanjo <ogunbanjotemiloluwa@gmail.com>
2024-02-29 22:51:01 +0100

    Dmnjd

--- app/fav/page.tsx
+++ app/fav/page.tsx
@@ -4,15 +4,18 @@ import TypeIt from "typeit-react";
 
 function Fave() {
   return (
-    <main className="flex min-h-screen flex-col items-start justify-center py-14 px-12 md:px-24 md:py-24 overflow-x-hidden bg-purple-950 text-2xl font-semibold">
+    <main className="flex min-h-screen flex-col items-start justify-center py-14 px-12 md:px-24 md:py-24 overflow-x-hidden bg-purple-950 text-xl font-normal">
       <TypeIt
         getBeforeInit={(instance) => {
           instance
             .type("Hiii Favy,", { speed: 10, })
             .break()
-            .type("Happy birthday baby girl, hope you forgive me for not calling you till now?", { speed: 10, })
             .break()
-            .type("I wanted to do something different this time around");
+            .type("Happy birthday baby girl, ", { speed: 10, })
+            .pause(750)
+            .type("hope you forgive me for not calling you till now?", { speed: 10, })
+            .break()
+            .type("I wanted to do something different this time around but nepa didn't let me complete it...");
 
           return instance;
         }}
